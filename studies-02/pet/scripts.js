2222550965d83699f1e64b508f21ce4f3af34558
Rômulo Henri <123867521+Henri2023@users.noreply.github.com>
2023-02-02 17:27:14 -0300

    Organizando 0.8.4

---
+++ studies-02/pet/scripts.js
@@ -0,0 +1,19 @@
+function start() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('img')
+    var date = new Date()
+    var hour = date.getHours()
+
+    console.log (`O horário é ${hour} horas`)
+
+    msg.innerHTML = `Agora são ${hour} horas`
+
+    if (hour >= 4 && hour < 12) {
+        img.src= 'assets/photo-morning.png'
+    } else if (hour >= 12 && hour < 18) {
+        img.src = 'assets/photo-evening.png'
+    } else {
+        img.src = 'assets/photo-night.png'
+    }
+    
+}
