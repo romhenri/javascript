ce84e3b794572804f2f14091d7ca12b9256245ea
Rômulo Henri <123867521+Henri2023@users.noreply.github.com>
2023-02-07 06:57:42 -0300

    Organizando 0.9.1

--- studies-02/registro-de-pet/script.js
+++ studies-02/registro-de-pet/script.js
@@ -1,31 +1,45 @@
-// function register() {
-//     console.log('Botão de Registro pressionado')
+function register() {
+    console.log('Botão de Registro pressionado')
 
-//     var date = new Date()
-//     var year = date.getFullYear()
-//     var syp = document.getElementById('st')
-//     var sy = Number(syp.value)
-//     var res = document.getElementById('conclusion')
-//     var time = year -sy
-//     var name = document.getElementById('n')
+    var date = new Date()
+    var year = date.getFullYear()
+    var syp = document.getElementById('st')
+    var sy = Number(syp.value)
+    var res = document.getElementById('conclusion')
+    var time = year -sy
+    var name = document.getElementById('n')
 
-//     /*console.log(date)
-//     console.log(year)
-//     console.log(syp)
-//     console.log(sy)
-//     console.log(n)*/
+    console.log(date)
+    console.log(year)
+    console.log(syp)
+    console.log(sy)
+    console.log(n)
     
 
-//     if (sy == 0 || sy.value > year) {
-//         console.log('Dados inválidos!')
-//         alert('Dados invalidos!')
-//     } else {
-//         var seg = document.querySelector('input[name=skin]:checked').value
-//         alert(seg)
-//         console.log(x)
-//     }}
+    if (sy == 0 || sy.value > year) {
+        console.log('Dados inválidos!')
+        alert('Dados invalidos!')
+    } else {
+        var buttonS = document.getElementsByName('skin')
+        var coat = ''
+        // var img = document.createElement('img')
+        // img.setAttribute('id', 'cat')
 
-function register() {
-    var seg = document.querySelector('input[name=skin]:checked').value
-    alert(seg)
-    console.log(seg)}
+        var img = document.getElementById('cat')
+
+        if (buttonS[0].checked) {
+            coat = 'golden'
+            img.setAttribute('src', 'golden-cat.png')
+        } else if (buttonS[1].checked) {
+            coat = 'gray'
+            img.setAttribute('src', 'gray-cat.png')
+        } else if (buttonS[2].checked) {
+            coat = 'brown'
+            img.setAttribute('src', 'brown-cat.png')
+        } else {
+            console.log('Pelagem não identificada!');
+        }
+
+        console.log(coat);
+    }
+}
---
+++ studies-02/sorteador/script.js
@@ -0,0 +1,22 @@
+var x = 0
+
+var now = window.document.getElementById('currentNumber')
+now.innerHTML = (x)
+
+function menos() {
+    var now = window.document.getElementById('currentNumber')
+    x = x - 1
+    now.innerHTML = (x)
+}
+
+function mais() {
+    var now = window.document.getElementById('currentNumber')
+    x = x +1
+    now.innerHTML = (x)
+}
+
+function reset() {
+    var now = window.document.getElementById('currentNumber')
+    x = 0
+    now.innerHTML = (x)
+}
