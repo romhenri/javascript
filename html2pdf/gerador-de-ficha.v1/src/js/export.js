1460e24f74cd3b62c6bbf0b44a61ef751d852c99
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-05-08 21:05:17 -0300

    add: html2pdf/gerador-de-ficha.v1

---
+++ html2pdf/gerador-de-ficha.v1/src/js/export.js
@@ -0,0 +1,35 @@
+var fileName = "arquivo";
+
+btnGenerate.addEventListener("click", () => {
+	console.log("Gerando...");
+
+	// PDF Name
+	if (inputName.value) {
+		fileName = inputName.value;
+	}
+
+	if (inputFormat.value == "jpeg") {
+		fileName = `${fileName}.jpeg`;
+	}
+
+	// PDF Content
+	const content = document.querySelector("#content");
+
+	// PDF Config
+	const options = {
+		margin: [0, 0, 0, 0],
+		filename: fileName,
+		image: { type: "jpeg", quality: 1 },
+		html2canvas: {
+			scale: 3,
+		},
+		jsPDF: {
+			unit: "in",
+			format: "a4",
+			orientation: "portrait",
+		},
+	};
+
+	// Generate PDF
+	html2pdf().set(options).from(content).save();
+});
