8d51a81b5b3ade3ae7b776db95f83d12d840d97a
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-05-29 22:00:35 -0300

    update: fabric.js/basic-photo-editor

---
+++ fabric.js/basic-photo-editor/src/js/fabric-style.js
@@ -0,0 +1,5 @@
+fabric.Object.prototype.transparentCorners = false
+// fabric.Object.prototype.cornerColor = 'green'
+fabric.Object.prototype.cornerStyle = 'circle'
+fabric.Object.prototype.transparentCorners = true
+fabric.Object.prototype.cornerSize = 20
