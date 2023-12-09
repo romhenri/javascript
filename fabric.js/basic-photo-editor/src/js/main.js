ac825d30084d3c412d0cebfb14060a4354f0f312
Rômulo Henri <romulohenri000@gmail.com>
2023-12-08 23:13:34 -0300

    basic-photo-editor-feat: Copy and past

--- fabric.js/basic-photo-editor/src/js/main.js
+++ fabric.js/basic-photo-editor/src/js/main.js
@@ -66,3 +66,40 @@ inputFile.addEventListener('change', function (e) {
 		picture.innerHTML = pictureImageTxt
 	}
 })
+
+
+btnCopy.addEventListener('click', () => {
+	// clone what are you copying since you
+	// may want copy and paste on different moment.
+	// and you do not want the changes happened
+	// later to reflect on the copy.
+	canvas.getActiveObject().clone(function(cloned) {
+		_clipboard = cloned;
+	});
+})
+
+btnPaste.addEventListener('click', () => {
+	// clone again, so you can do multiple copies.
+	_clipboard.clone(function(clonedObj) {
+		canvas.discardActiveObject();
+		clonedObj.set({
+			left: clonedObj.left + 10,
+			top: clonedObj.top + 10,
+			evented: true,
+		});
+		if (clonedObj.type === 'activeSelection') {
+			// active selection needs a reference to the canvas.
+			clonedObj.canvas = canvas;
+			clonedObj.forEachObject(function(obj) {
+				canvas.add(obj);
+			});
+			// this should solve the unselectability
+			clonedObj.setCoords();
+		} else {
+			canvas.add(clonedObj);
+		}
+		_clipboard.top += 10;
+		_clipboard.left += 10;
+		canvas.setActiveObject(clonedObj);
+		canvas.requestRenderAll();})
+})
